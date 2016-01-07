0c6abb8959da765bfcfc88ceddace6a6026df0c9
megha iyer <connecteduqa@gmail.com>
2016-01-07 14:40:47 -0600

    one more ignore

--- test/com/megha/junittest/VerifySendGmail.java
+++ test/com/megha/junittest/VerifySendGmail.java
@@ -149,7 +149,7 @@ public class VerifySendGmail {
 	e.printStackTrace();
 }
 }
-	
+	@Ignore
 	@Test(timeout=1000)
 	public void testPrintParameters(){
 		System.out.println("Subject:"+Subject);
@@ -159,6 +159,7 @@ public class VerifySendGmail {
 		
 	}
 	
+	@Ignore
 	@Test
 	public void testHelloUser(){
 		
